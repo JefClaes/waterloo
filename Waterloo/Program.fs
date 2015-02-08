17e278a8e5ee4cf8bdcbc86c2e58d6ca5c60a8a8
Jef Claes <jef.claes@gmail.com>
2015-02-08 18:42:29 +0100

    Missing files

---
+++ Waterloo/Program.fs
@@ -0,0 +1,69 @@
+﻿open System
+open System.IO
+open System.Threading
+open Waterloo.Napoleon
+open Waterloo.Bingoal
+open WaterlooModule
+
+[<EntryPoint>]
+let main argv = 
+    let run () =
+        let napoleonFeed = NapoleonFeed.Fetch "https://e3-api.kambi.com/offering/api/v2/ngbe/event/live/open.json"
+        let bingoalFeed = BingoalFeed.Fetch "https://www.bingoal.be/generated/livemenu1.js"
+    
+        let printFeed ( feed : Feed ) =
+            Console.ForegroundColor <- ConsoleColor.Yellow
+            printfn "Bookie = %A" feed.Bookie
+            Console.ForegroundColor <- ConsoleColor.White 
+            printfn "%67s %10s - %10s" String.Empty "1" "2"
+            for e in feed.Events do
+                printfn "%30s VS %30s => %10i - %10i" e.Home e.Away e.Bet.Odds1 e.Bet.Odds2 
+        
+        let printMatches ( matchedEvents : seq<MatchedEvent> ) =
+            Console.ForegroundColor <- ConsoleColor.Yellow 
+            printfn "Matched events"
+            Console.ForegroundColor <- ConsoleColor.Green
+            for e in matchedEvents do
+            (
+                printfn "%30s VS %30s" e.Home e.Away 
+                printfn "%23s %10s - %10s" String.Empty "1" "2"
+                for b in e.Bets do
+                    printfn "%20A => %10i - %10i" b.Bookie b.Bet.Odds1 b.Bet.Odds2
+            )
+
+        let printArbitraged ( arbitrageResults : seq<ArbitrageResult> ) =
+            Console.ForegroundColor <- ConsoleColor.Yellow
+            printfn "Arbitraged events %A" (arbitrageResults |> Seq.length)
+            Console.ForegroundColor <- ConsoleColor.Green
+            if ((arbitrageResults |> Seq.length) > 0) then
+                  for i in 1 .. 5 do
+                    Console.Beep(1500, 1000);
+            for r in arbitrageResults do
+                printfn "%A" r
+
+        let napoleonEvents = convertNapoleonFeed napoleonFeed
+        let bingoalEvents = convertBingoalFeed bingoalFeed
+    
+        printFeed napoleonEvents
+        printFeed bingoalEvents
+
+        let matchedEvents = matchFeeds napoleonEvents bingoalEvents
+
+        printMatches matchedEvents
+
+        let arbitraged = arbitrage matchedEvents
+
+        printArbitraged arbitraged
+
+    while true do
+        Console.Beep()
+        run()
+        Console.ForegroundColor <- ConsoleColor.White
+        let wait = 1000 * 60
+        printfn ""
+        printfn "Retrying in %A ms..." wait
+        printfn ""
+        System.Threading.Thread.Sleep(wait)
+
+    printfn "%A" argv
+    0 // return an integer exit code
---
+++ Waterloo/WaterlooModule.fs
@@ -0,0 +1,91 @@
+﻿module WaterlooModule
+
+    open System
+    
+    type Bookie = | Napoleon | Bingoal
+    type Bet = { Odds1 : int; Odds2 : int  }
+    type Event = { Home : string; Away : string; Bet : Bet }
+    type Events = seq<Event>
+    type Feed = { Bookie : Bookie; Events : Events }
+        
+    let convertNapoleonFeed ( napoleonFeed : Waterloo.Napoleon.RootObject ) = 
+        let events = 
+            napoleonFeed.liveEvents        
+            |> Seq.filter (fun x -> x.event.sport = "TENNIS")
+            |> Seq.filter (fun x -> x.mainBetOffer <> null && x.mainBetOffer.outcomes <> null && x.mainBetOffer.outcomes |> Seq.length > 0)
+            |> Seq.map (fun x ->                
+                let extractOutcome ( option : Option<'a> ) =
+                    match option with
+                    | Option.None -> failwith "Outcome not found"
+                    | Option.Some x -> x
+                            
+                let bet1 = x.mainBetOffer.outcomes |> Seq.tryFind (fun x -> x.label = "1") |> extractOutcome
+                let bet2 = x.mainBetOffer.outcomes |> Seq.tryFind (fun x -> x.label = "2") |> extractOutcome
+
+                { Home = x.event.homeName; Away = x.event.awayName; Bet = { Odds1 = bet1.odds; Odds2 = bet2.odds } })                                               
+        { Bookie = Napoleon; Events = events }
+            
+    let convertBingoalFeed ( bingoalFeed : Waterloo.Bingoal.RootObject ) =
+        let tennis = 
+            bingoalFeed.sports 
+            |> Seq.filter (fun x -> x.sport = "TENNIS") 
+            |> Seq.nth 0
+        let options = 
+            tennis.bets 
+            |> Seq.map (fun x -> x.subbets |> Seq.filter (fun y -> y.active && y.bettype = 44) |> Seq.map (fun z -> z.options))
+            |> Seq.concat 
+        let events = 
+            options 
+            |> Seq.map (fun x -> 
+                (
+                    let quoteToOdd (quot : string) = 
+                        Convert.ToInt32(Convert.ToDecimal(quot.Replace(".", ",")) * 1000M) 
+
+                    let o = x |> Seq.toList
+                    let home = o |> Seq.nth 0
+                    let away = o |> Seq.nth 1
+                    
+                    { 
+                        Home = home.name
+                        Away = away.name                                 
+                        Bet = { Odds1 = home.quot |> quoteToOdd; Odds2 = away.quot |> quoteToOdd }                       
+                    }
+                ))
+        { Bookie = Bingoal; Events = events }
+        
+    type MatchedBet = { Bet : Bet; Bookie : Bookie; }
+    type MatchedEvent = { Home : string; Away : string; Bets : seq<MatchedBet> }     
+
+    let matchFeeds ( napoleon : Feed ) ( bingoal : Feed ) = 
+        napoleon.Events 
+        |> Seq.map (fun ne -> 
+            let matchingEvent = 
+                bingoal.Events 
+                |> Seq.tryFind (fun x -> x.Home = ne.Home && x.Away = ne.Away)
+            match matchingEvent with
+            | Some m -> Option.Some { 
+                Home = m.Home; 
+                Away = m.Away; 
+                Bets = [ { Bet = ne.Bet; Bookie = napoleon.Bookie }; { Bet = m.Bet; Bookie = bingoal.Bookie } ]}                
+            | None -> Option.None )
+        |> Seq.filter (fun x -> x.IsSome)
+        |> Seq.map (fun x -> x.Value)
+
+    type ArbitrageResult = { Event : MatchedEvent; Discrepancy1 : float; Discrepancy2 : float }
+
+    let arbitrage ( events : seq<MatchedEvent> ) =
+        events 
+        |> Seq.map (fun e ->
+            let napoleonBet = e.Bets |> Seq.where (fun x -> x.Bookie = Bookie.Napoleon) |> Seq.nth 0
+            let bingoalBet = e.Bets |> Seq.where (fun x -> x.Bookie = Bookie.Bingoal) |> Seq.nth 0
+            
+            let discrepancy1 = Math.Pow(float napoleonBet.Bet.Odds1 / float 1000, float -1) + Math.Pow(float bingoalBet.Bet.Odds2 / float 1000, float -1) 
+            let discrepancy2 = Math.Pow(float napoleonBet.Bet.Odds2 / float 1000, float -1) + Math.Pow(float bingoalBet.Bet.Odds1 / float 1000, float -1)
+
+            let success = discrepancy1 < float 1 || discrepancy2 < float 1
+            
+            match success with
+            | true -> Option.Some { Event = e; Discrepancy1 = discrepancy1; Discrepancy2 = discrepancy2 }
+            | false -> Option.None)
+        |> Seq.filter (fun x -> x.IsSome)
+        |> Seq.map (fun x -> x.Value)
